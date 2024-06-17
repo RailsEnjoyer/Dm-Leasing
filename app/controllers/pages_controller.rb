2d2c98a9e266e7c7c5684b2380ff6a923ebdef3a
railsenjoyer <railsenjoyer@gmail.com>
2024-06-17 23:44:08 +0300

    added contacts page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def new_cars
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  get 'new_cars', to: 'pages#new_cars'
+  get 'contact', to: 'pages#contact'
 end
